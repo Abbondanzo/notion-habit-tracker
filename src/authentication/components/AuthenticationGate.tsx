1b0095e96d3c67a5e3513673dc94827f9f3d35a4
Abbondanzo <peter@abbondanzo.com>
2022-02-02 23:57:29 -0500

    Style gate

--- src/authentication/components/AuthenticationGate.tsx
+++ src/authentication/components/AuthenticationGate.tsx
@@ -1,9 +1,21 @@
 import { Client } from "@notionhq/client";
 import { PropsWithChildren, useMemo } from "react";
+import styled from "styled-components";
 import { AuthenticationContext } from "../context/AuthenticationContext";
 
+const BlankRoot = styled.div`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+
 export const AuthenticationGate = ({ children }: PropsWithChildren<{}>) => {
-  const notionAPIKey = process.env.NOTION_API_KEY;
+  const notionAPIKey = process.env.REACT_APP_NOTION_API_KEY;
   const notionClient = useMemo(() => {
     return new Client({
       auth: notionAPIKey,
@@ -12,10 +24,10 @@ export const AuthenticationGate = ({ children }: PropsWithChildren<{}>) => {
 
   if (!notionAPIKey) {
     return (
-      <div>
-        No Notion API key could be found. Please add <code>NOTION_API_KEY</code>{" "}
-        to the environment and try again
-      </div>
+      <BlankRoot>
+        No Notion API key could be found. Please add{" "}
+        <code>REACT_APP_NOTION_API_KEY</code> to the environment and try again
+      </BlankRoot>
     );
   }
 
