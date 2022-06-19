7d965a203d35a81c06e2eee1dd86eb15ea25bdee
Abbondanzo <peter@abbondanzo.com>
2022-06-19 14:05:06 +0200

    Check for dev environment first on router

--- packages/server/src/router.ts
+++ packages/server/src/router.ts
@@ -12,11 +12,11 @@ interface Context {
 export const createContext = async ({
   req,
 }: CreateExpressContextOptions): Promise<Context> => {
-  if (req.headers.authorization) {
+  if (isDev) {
+    return { provider: new FilesystemProvider() };
+  } else if (req.headers.authorization) {
     const client = new Client({ auth: req.headers.authorization });
     return { provider: new NotionProvider(client) };
-  } else if (isDev) {
-    return { provider: new FilesystemProvider() };
   } else {
     throw new TRPCError({
       code: "UNAUTHORIZED",
@@ -27,12 +27,27 @@ export const createContext = async ({
 
 type AuthenticatedContext = NonNullable<Context>;
 
-const databases = router<AuthenticatedContext>().query("getDatabase", {
-  input: z.object({ databaseId: z.string() }),
-  resolve: async ({ ctx, input }) => {
-    return ctx.provider.databaseRepository.getDatabase(input.databaseId);
-  },
-});
+const databases = router<AuthenticatedContext>()
+  .query("getDatabase", {
+    input: z.object({ databaseId: z.string() }),
+    resolve: async ({ ctx, input }) => {
+      const database = await ctx.provider.databaseRepository.getDatabase(
+        input.databaseId
+      );
+      if (!database) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: "Could not find a database by the given ID",
+        });
+      }
+      return database;
+    },
+  })
+  .query("getDatabases", {
+    resolve: async ({ ctx }) => {
+      return ctx.provider.databaseRepository.getDatabases();
+    },
+  });
 
 export const appRouter = router<Context>().merge("databases/", databases);
 
